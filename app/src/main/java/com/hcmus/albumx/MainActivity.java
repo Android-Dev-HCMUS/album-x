cf8535569611b53dc583bc2d0366ddac3427efa7
pmtoan <contact.pmtoan@gmail.com>
2022-03-24 13:22:37 +0700

    New Merge

--- app/src/main/java/com/hcmus/albumx/MainActivity.java
+++ app/src/main/java/com/hcmus/albumx/MainActivity.java
@@ -1,88 +1,50 @@
 package com.hcmus.albumx;
 
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.hcmus.albumx.AlbumList.AlbumList;
+import com.hcmus.albumx.AllPhotos.AllPhotos;
 
-public class MainActivity extends AppCompatActivity {
-
-    private ViewPager mViewPager;
-    private BottomNavigationView mBottomNavigationView;
-
+public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_interface_new);
-//        setContentView(R.layout.image_editing);
-//        setContentView(R.layout.image_viewing);
+        setContentView(R.layout.activity_main);
 
-        mViewPager = findViewById(R.id.view_pager);
-        mBottomNavigationView = findViewById(R.id.bottom);
+        loadFragment(AllPhotos.newInstance("main_layout"), "AllPhotos", "AllPhotosUI");
 
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        mViewPager.setAdapter(adapter);
+        ImageButton button = (ImageButton) findViewById(R.id.addBtn);
 
-        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.menu_album:
+                    button.setVisibility(View.INVISIBLE);
+                    loadFragment(AlbumList.newInstance("album"), "AlbumList", "AlbumListUI");
+                    break;
+                case R.id.menu_photo:
+                default:
+                    button.setVisibility(View.VISIBLE);
+                    loadFragment(AllPhotos.newInstance("main_layout"), "AllPhotos", "AllPhotosUI");
+                    break;
             }
 
-            @Override
-            public void onPageSelected(int position) {
-                switch (position) {
-                    case 0:
-                        mBottomNavigationView.getMenu().findItem(R.id.buttonEdit).setChecked(true);
-                        break;
-                    case 1:
-                        mBottomNavigationView.getMenu().findItem(R.id.buttonPhoto).setChecked(true);
-                        break;
-                    case 2:
-                        mBottomNavigationView.getMenu().findItem(R.id.buttonAlbum).setChecked(true);
-                        break;
-                    case 3:
-                        mBottomNavigationView.getMenu().findItem(R.id.buttonTrash).setChecked(true);
-                        break;
-                    case 4:
-                        mBottomNavigationView.getMenu().findItem(R.id.buttonInformation).setChecked(true);
-                        break;
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
+            return true;
         });
+    }
 
-        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()) {
-                    case R.id.buttonEdit:
-                        mViewPager.setCurrentItem(0);
-                        break;
-                    case R.id.buttonPhoto:
-                        mViewPager.setCurrentItem(1);
-                        break;
-                    case R.id.buttonAlbum:
-                        mViewPager.setCurrentItem(2);
-                        break;
-                    case R.id.buttonTrash:
-                        mViewPager.setCurrentItem(3);
-                        break;
-                    case R.id.buttonInformation:
-                        mViewPager.setCurrentItem(4);
-                        break;
-                }
-                return true;
-            }
-        });
+    private void loadFragment(Fragment fragment, String TAG, String name){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameFragment, fragment, TAG)
+                .addToBackStack(name)
+                .commit();
     }
 }
