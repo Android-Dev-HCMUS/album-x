a2e17eb849b8efd619e07eb8b74940973f85d40c
nddat1811 <nddat1811@gmail.com>
2022-03-26 22:09:56 +0700

    .....

---
+++ album-X/app/build/generated/source/buildConfig/debug/com/photoeditorsdk/android/app/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.photoeditorsdk.android.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.photoeditorsdk.android.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final boolean IMGLY_ENABLED = true;
+      
+      @ly.img.android.pesdk.annotations.StateEvents private static class IMGLY_PROCESSOR_INIT extends com.photoeditorsdk.android.app.IMGLYEvents {IMGLY_PROCESSOR_INIT(){super(null);}};
+}
---
+++ album-X/app/build/generated/source/kapt/debug/com/photoeditorsdk/android/app/events/$EventAccessorMap.java
@@ -0,0 +1,61 @@
+package com.photoeditorsdk.android.app.events;
+import java.util.HashMap;
+@SuppressWarnings("deprecation")
+@ly.img.android.pesdk.annotations.EventDispatcher
+public final class $EventAccessorMap {
+    public static final HashMap<Class<?>, Class<?>> classAccessorsMap = ly.img.android.events.$EventAccessorMap.classAccessorsMap;
+
+    static {
+            
+            ly.img.android.pesdk.backend.filter.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.brush.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.adjustment.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.adjustment.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.text.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.overlay.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.frame.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.text_design.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.focus.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.filter.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.sticker.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.frame.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.text_design.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.sticker.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.all.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.transform.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.brush.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.overlay.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.sticker_smart.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.text.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.video_core.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.ui.events.$EventAccessorMap.classAccessorsMap.size();
+                
+            ly.img.android.pesdk.backend.camera.events.$EventAccessorMap.classAccessorsMap.size();
+                 
+         
+    
+    }
+}
